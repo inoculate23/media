fa816dbf826d36ba376d4a32f9a4051a75c36b70
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2022-03-15 19:47:28 -0700

    Update webgl-demo.js

--- fred_bday/js/webgl-demo.js
+++ fred_bday/js/webgl-demo.js
@@ -83,7 +83,7 @@ function main() {
 
     const texture = initTexture(gl);
 
-    const video = setupVideo('https://inoculate.media/assets/images/inoculate_512.mp4');
+    const video = setupVideo('https://inoculate.media/fred_bday/textures/all_2.mp4');
 
     var then = 0;
 
