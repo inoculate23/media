c039d76ac398ece97a581fce26720c2db5ab945e
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2022-06-07 16:22:20 -0700

    Delete rtc directory

--- rtc/request.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-response = requests.get('https://go.xxxiijmp.com/api/models/')
-data = response.json()
-for i in data['models']:
-            append(i)
-            print(f"\"{i['url']}\" {i['username'][0]['snapshotUrl']}")
-            
-            print(response.json())
