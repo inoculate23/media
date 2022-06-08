e29af58cb1a8736a31b45f648095fcd4ace2a969
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2022-06-07 17:03:04 -0700

    Add files via upload

--- rtc_main/main.js
+++ rtc_main/main.js
@@ -19,11 +19,11 @@ function gotLocalMediaStream(mediaStream) {
 
 // Handles error by logging a message to the console with the error message.
 function handleLocalMediaStreamError(error) {
-  console.log('navigator.getDisplayMedia error: ', error);
+  console.log('navigator.getUserMedia error: ', error);
 }
 
 // Initializes media stream.
-navigator.mediaDevices.getDisplayMedia(mediaStreamConstraints)
+navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
   .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
 
   'use strict';
