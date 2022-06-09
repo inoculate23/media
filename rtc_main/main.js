8872e9dfb7432416a533b1331b6191a2d3905948
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2022-06-09 04:17:57 -0700

    Add files via upload

--- rtc_main/main.js
+++ rtc_main/main.js
@@ -248,3 +248,20 @@ socket.on('log', function(array) {
 });
   
   
+const remoteVideo = document.querySelector('#remoteVideo');
+
+peerConnection.addEventListener('track', async (event) => {
+    const [remoteStream] = event.streams;
+    remoteVideo.srcObject = remoteStream;
+});
+
+async function playVideoFromCamera() {
+  try {
+      const constraints = {'video': true, 'audio': true};
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      const videoElement = document.querySelector('video#localVideo');
+      videoElement.srcObject = stream;
+  } catch(error) {
+      console.error('Error opening video camera.', error);
+  }
+}
