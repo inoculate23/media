4aaaa7a2842e66da75e65f0faf08de57c8e26545
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-06-20 01:51:42 -0700

    Add files via upload

---
+++ rtc_main/kinesis_tests/stream/js/app.js
@@ -0,0 +1,435 @@
+let ROLE = null; // Possible values: 'master', 'viewer', null
+const LOG_LEVELS = ['info', 'warn', 'error'];
+let LOG_LEVEL = 'info'; // Possible values: any value of LOG_LEVELS
+
+function configureLogging() {
+    function log(level, messages) {
+        const text = messages
+            .map(message => {
+                if (message instanceof Error) {
+                    const { stack, ...rest } = message;
+                    if (Object.keys(rest).length === 0) {
+                        if (stack) {
+                            return stack;
+                        } else {
+                            return message;
+                        }
+                    }
+                    return `${JSON.stringify(rest, null, 2)}\n${stack}`;
+                } else if (typeof message === 'object') {
+                    return JSON.stringify(message, null, 2);
+                } else if (message === undefined) {
+                    return 'undefined';
+                } else {
+                    return message;
+                }
+            })
+            .join(' ');
+
+        const logLine = $(`<div class="${level.toLowerCase()}">`).text(`[${new Date().toISOString()}] [${level}] ${text}\n`);
+        if (LOG_LEVELS.indexOf(LOG_LEVEL) > LOG_LEVELS.indexOf(level.toLowerCase())) {
+            logLine.addClass('d-none');
+        }
+        $('#logs').append(logLine);
+        const logsContainer = document.getElementById('logs');
+        logsContainer.scrollTo(0, logsContainer.scrollHeight);
+    }
+
+    console._error = console.error;
+    console.error = function(...rest) {
+        log('ERROR', Array.prototype.slice.call(rest));
+        console._error.apply(this, rest);
+    };
+
+    console._warn = console.warn;
+    console.warn = function(...rest) {
+        log('WARN', Array.prototype.slice.call(rest));
+        console._warn.apply(this, rest);
+    };
+
+    console._log = console.log;
+    console.log = function(...rest) {
+        log('INFO', Array.prototype.slice.call(rest));
+        console._log.apply(this, rest);
+    };
+}
+
+function getRandomClientId() {
+    return Math.random()
+        .toString(36)
+        .substring(2)
+        .toUpperCase();
+}
+
+function getFormValues() {
+    return {
+        region: $('#region').val(),
+        channelName: $('#channelName').val(),
+        clientId: $('#clientId').val() || getRandomClientId(),
+        sendVideo: $('#sendVideo').is(':checked'),
+        sendAudio: $('#sendAudio').is(':checked'),
+        openDataChannel: $('#openDataChannel').is(':checked'),
+        widescreen: $('#widescreen').is(':checked'),
+        fullscreen: $('#fullscreen').is(':checked'),
+        useTrickleICE: $('#useTrickleICE').is(':checked'),
+        natTraversalDisabled: $('#natTraversalDisabled').is(':checked'),
+        forceTURN: $('#forceTURN').is(':checked'),
+        accessKeyId: $('#accessKeyId').val(),
+        endpoint: $('#endpoint').val() || null,
+        secretAccessKey: $('#secretAccessKey').val(),
+        sessionToken: $('#sessionToken').val() || null,
+        ingestMedia: $('#ingestMedia').is(':checked'),
+        enableDQPmetrics: $('#enableDQPmetrics').is(':checked'),
+    };
+}
+
+function toggleDataChannelElements() {
+    if (getFormValues().openDataChannel) {
+        $('.datachannel').removeClass('d-none');
+    } else {
+        $('.datachannel').addClass('d-none');
+    }
+}
+
+function onStatsReport(report) {
+    // TODO: Publish stats
+}
+
+function onStop() {
+    if (!ROLE) {
+        return;
+    }
+
+    if (ROLE === 'master') {
+        stopMaster();
+        $('#master').addClass('d-none');
+    } else {
+        stopViewer();
+        $('#viewer').addClass('d-none');
+    }
+
+    if (getFormValues().enableDQPmetrics) {
+        $('#dqpmetrics').addClass('d-none');
+        $('#webrtc-live-stats').addClass('d-none');
+    }
+
+    $('#form').removeClass('d-none');
+    ROLE = null;
+}
+
+window.addEventListener('beforeunload', onStop);
+
+window.addEventListener('error', function(event) {
+    console.error(event.message);
+    event.preventDefault();
+});
+
+window.addEventListener('unhandledrejection', function(event) {
+    console.error(event.reason.toString());
+    event.preventDefault();
+});
+
+configureLogging();
+
+$('#master-button').click(async () => {
+    const form = $('#form');
+    if (!form[0].checkValidity()) {
+        return;
+    }
+    if (!checkWebRTCStorageRequirements()) {
+        console.error('Both Send Video and Send Audio checkboxes need to be checked to ingest media.');
+        return;
+    }
+    ROLE = 'master';
+    form.addClass('d-none');
+    $('#master').removeClass('d-none');
+
+    const localView = $('#master .local-view')[0];
+    const remoteView = $('#master .remote-view')[0];
+    const localMessage = $('#master .local-message')[0];
+    const remoteMessage = $('#master .remote-message')[0];
+    const formValues = getFormValues();
+
+    $(remoteMessage).empty();
+    localMessage.value = '';
+    toggleDataChannelElements();
+
+    startMaster(localView, remoteView, formValues, onStatsReport, event => {
+        remoteMessage.append(`${event.data}\n`);
+    });
+});
+
+$('#clear-logs').click(() => {
+    $('#logs').empty();
+});
+
+$('#stop-master-button').click(onStop);
+
+$('#viewer-button').click(async () => {
+    const form = $('#form');
+    if (!form[0].checkValidity()) {
+        return;
+    }
+    if (!checkWebRTCStorageRequirements()) {
+        console.error('Both Send Video and Send Audio checkboxes need to be checked to ingest media.');
+        return;
+    }
+    ROLE = 'viewer';
+    form.addClass('d-none');
+    $('#viewer').removeClass('d-none');
+
+    const localView = $('#viewer .local-view')[0];
+    const remoteView = $('#viewer .remote-view')[0];
+    const localMessage = $('#viewer .local-message')[0];
+    const remoteMessage = $('#viewer .remote-message')[0];
+    const formValues = getFormValues();
+
+    if (formValues.enableDQPmetrics) {
+        $('#dqpmetrics').removeClass('d-none');
+        $('#webrtc-live-stats').removeClass('d-none');
+    }
+
+    $(remoteMessage).empty();
+    localMessage.value = '';
+    toggleDataChannelElements();
+
+    startViewer(localView, remoteView, formValues, onStatsReport, event => {
+        remoteMessage.append(`${event.data}\n`);
+    });
+});
+
+$('#stop-viewer-button').click(onStop);
+
+$('#create-channel-button').click(async () => {
+    const formValues = getFormValues();
+
+    createSignalingChannel(formValues);
+});
+
+$('#master .send-message').click(async () => {
+    const masterLocalMessage = $('#master .local-message');
+    if (sendMasterMessage(masterLocalMessage.val())) {
+        masterLocalMessage.val('');
+    }
+});
+
+$('#viewer .send-message').click(async () => {
+    const viewerLocalMessage = $('#viewer .local-message');
+    if (sendViewerMessage(viewerLocalMessage.val())) {
+        viewerLocalMessage.val('');
+    }
+});
+
+$('#more-logs').click(async () => {
+    const logElement = $('#logs');
+    logElement.height(logElement.height() + 50);
+});
+
+$('#less-logs').click(async () => {
+    const logElement = $('#logs');
+    logElement.height(Math.max(100, logElement.height() - 50));
+});
+
+async function logLevelSelected(event) {
+    LOG_LEVEL = event.target.getAttribute('data-level').toLowerCase();
+
+    // Change which button is selected
+    for (const child of $('#tabs').children()) {
+        child.setAttribute('class', event.target.id === child.id ? 'btn btn-primary' : 'btn btn-light');
+    }
+
+    // Make the logs hidden and shown based on the selected level
+    $('#logs > div').each((idx, child) => {
+        if (LOG_LEVELS.indexOf(LOG_LEVEL) <= LOG_LEVELS.indexOf(child.classList[0])) {
+            child.classList.remove('d-none');
+        } else {
+            child.classList.add('d-none');
+        }
+    });
+}
+
+// Fetch regions
+fetch('https://api.regional-table.region-services.aws.a2z.com/index.jsons')
+    .then(res => {
+        if (res.ok) {
+            return res.json();
+        }
+        return Promise.reject(`${res.status}: ${res.statusText}`);
+    })
+    .then(data => {
+        data?.prices
+            ?.filter(serviceData => serviceData?.attributes['aws:serviceName'] === 'Amazon Kinesis Video Streams')
+            .map(kinesisVideoServiceData => kinesisVideoServiceData?.attributes['aws:region'])
+            .sort()
+            .forEach(region => {
+                $('#regionList').append(
+                    $('<option>', {
+                        value: region,
+                        text: region,
+                    }),
+                );
+            });
+        $('#region').attr('list', 'regionList');
+        console.log('[FETCH-REGIONS] Successfully fetched regions!');
+    })
+    .catch(err => {
+        console.error('[FETCH-REGIONS] Encountered error fetching regions', err);
+    });
+
+// Region verification
+$('#region').on('focusout', event => {
+    const region = event.target.value;
+    let found = false;
+    let anyRegions = false;
+    for (const child of $('dataList').children()) {
+        anyRegions = true;
+        if (child.value === region) {
+            found = true;
+            break;
+        }
+    }
+    if (!anyRegions) {
+        return;
+    }
+
+    const regionElement = $('#region');
+
+    if (found) {
+        regionElement.addClass('is-valid');
+        regionElement.removeClass('is-invalid');
+    } else {
+        if (!region) {
+            $('#region-invalid-feedback').text('Please enter a region!');
+        } else {
+            // The dataset used mentions that it does not guarantee accuracy. In the case that
+            // it does not contain a certain region needed, we can still input regions needed.
+            $('#region-invalid-feedback').text('This region is not in the list of fetched regions!');
+            console.warn(`[REGION-VALIDATION] The region entered: \"${region}\" may be invalid!`);
+        }
+
+        regionElement.addClass('is-invalid');
+        regionElement.removeClass('is-valid');
+    }
+});
+
+// Audio/Video checkbox validation with WebRTC Storage
+function checkWebRTCStorageRequirements() {
+    const audio = $('#sendAudio');
+    const video = $('#sendVideo');
+    const ingestMedia = $('#ingestMedia');
+    if (ingestMedia.is(':checked')) {
+        let good = true;
+        if (!audio.is(':checked')) {
+            good = false;
+            audio.addClass('is-invalid');
+        } else {
+            audio.removeClass('is-invalid');
+        }
+        if (!video.is(':checked')) {
+            good = false;
+            video.addClass('is-invalid');
+        } else {
+            video.removeClass('is-invalid');
+        }
+        if (!good) {
+            ingestMedia.addClass('is-invalid');
+            return false;
+        }
+
+        ingestMedia.removeClass('is-invalid');
+    } else {
+        audio.removeClass('is-invalid');
+        video.removeClass('is-invalid');
+        ingestMedia.removeClass('is-invalid');
+    }
+    return true;
+}
+
+$('#sendAudio').click(() => {
+    checkWebRTCStorageRequirements();
+});
+$('#sendVideo').click(() => {
+    checkWebRTCStorageRequirements();
+});
+$('#ingestMedia').click(() => {
+    checkWebRTCStorageRequirements();
+});
+
+// Read/Write all of the fields to/from localStorage so that fields are not lost on refresh.
+const urlParams = new URLSearchParams(window.location.search);
+const fields = [
+    { field: 'channelName', type: 'text' },
+    { field: 'clientId', type: 'text' },
+    { field: 'region', type: 'text' },
+    { field: 'accessKeyId', type: 'text' },
+    { field: 'secretAccessKey', type: 'text' },
+    { field: 'sessionToken', type: 'text' },
+    { field: 'endpoint', type: 'text' },
+    { field: 'sendVideo', type: 'checkbox' },
+    { field: 'sendAudio', type: 'checkbox' },
+    { field: 'widescreen', type: 'radio', name: 'resolution' },
+    { field: 'fullscreen', type: 'radio', name: 'resolution' },
+    { field: 'openDataChannel', type: 'checkbox' },
+    { field: 'useTrickleICE', type: 'checkbox' },
+    { field: 'natTraversalEnabled', type: 'radio', name: 'natTraversal' },
+    { field: 'forceTURN', type: 'radio', name: 'natTraversal' },
+    { field: 'natTraversalDisabled', type: 'radio', name: 'natTraversal' },
+    { field: 'ingestMedia', type: 'checkbox' },
+];
+fields.forEach(({ field, type, name }) => {
+    const id = '#' + field;
+
+    // Read field from localStorage
+    try {
+        const localStorageValue = localStorage.getItem(field);
+        if (localStorageValue) {
+            if (type === 'checkbox' || type === 'radio') {
+                $(id).prop('checked', localStorageValue === 'true');
+            } else {
+                $(id).val(localStorageValue);
+            }
+            $(id).trigger('change');
+        }
+    } catch (e) {
+        /* Don't use localStorage */
+    }
+
+    // Read field from query string
+    if (urlParams.has(field)) {
+        paramValue = urlParams.get(field);
+        if (type === 'checkbox' || type === 'radio') {
+            $(id).prop('checked', paramValue === 'true');
+        } else {
+            $(id).val(paramValue);
+        }
+    }
+
+    // Write field to localstorage on change event
+    $(id).change(function() {
+        try {
+            if (type === 'checkbox') {
+                localStorage.setItem(field, $(id).is(':checked'));
+            } else if (type === 'radio') {
+                fields
+                    .filter(fieldItem => fieldItem.name === name)
+                    .forEach(fieldItem => {
+                        localStorage.setItem(fieldItem.field, fieldItem.field === field);
+                    });
+            } else {
+                localStorage.setItem(field, $(id).val());
+            }
+        } catch (e) {
+            /* Don't use localStorage */
+        }
+    });
+});
+
+// Enable tooltips
+$(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+// The page is all setup. Hide the loading spinner and show the page content.
+$('.loader').addClass('d-none');
+$('#main').removeClass('d-none');
+console.log('Page loaded');
