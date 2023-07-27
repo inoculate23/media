78d60f3d36eb02981228779797b672012284c2db
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-07-27 09:51:30 -0700

    Add files via upload

---
+++ assets/haawke_spinner/sketch.js
@@ -0,0 +1,82 @@
+let shiny;
+let fontBold;
+
+
+function setup() {
+  createCanvas(1920, 1080, WEBGL);
+  frameRate(60);
+
+
+  smooth();
+ 
+   img2 = loadImage('logo.webp');
+
+
+
+ img5=loadImage('5.jpeg');
+
+
+}
+function draw() {
+  background(10);
+
+
+
+  
+
+  specularMaterial(150, 230, 250);
+   
+
+  
+  let locX = mouseX - height / 2;
+  let locY = mouseY - width / 2;
+
+
+  pointLight(255, 33, 10, locX, locY, 200);
+  rotateX(frameCount * 0.005);
+  rotateY(frameCount * 0.002);
+    push();
+  rotateZ(frameCount * 0.005);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.003);
+
+  push()
+  // Set the material
+  
+ specularMaterial(250, 200, 200, 200);
+
+ ambientLight(10, 0,9)
+   texture(img5);
+   pointLight(25, 233, 210, locX, locY, 200);
+    box(204, 200);
+  shiny;
+  smooth()
+   pop()
+
+  pop();
+
+   ambientMaterial(150, 0, 57);
+  texture(img2);
+  textureMode(NORMAL);
+  beginShape();
+  vertex(-75, -75, 0, 0);
+  vertex(75, 75, 4, 0);
+  vertex(75, 75, 4, 4);
+  vertex(-75, 75, 0, 4);
+  endShape();
+
+        ambientLight(25,15,00)
+
+  directionalLight(255, 0, 200, 40.25, 70.25, 0);
+      box(424, 420, 600);  
+ push();
+  strokeWeight(9);
+  normalMaterial();
+
+
+}
+
+
+
+
+
