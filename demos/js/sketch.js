f62317a20303bc31eae8d2e324442af7fdc3df4b
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-11-01 12:23:18 -0700

    Add files via upload

--- demos/js/sketch.js
+++ demos/js/sketch.js
@@ -1,5 +1,5 @@
 let mic, fft;
-
+let windowResized;
 function setup() {
   createCanvas(710, 400);
   fill(23, 166, 199);
@@ -9,10 +9,18 @@ function setup() {
   fft = new p5.FFT();
   fft.setInput(mic);
 }
+function setup() {
+  createCanvas(windowWidth, 250);
+}
+
+
 
 function draw() {
   background(31, 33, 36);
 
+  function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+  }
   let spectrum = fft.analyze();
 
   beginShape();
