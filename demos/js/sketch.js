9cfeb3c3d1630aee70a9c6e1ecca9764de1fe168
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-11-01 13:35:44 -0700

    Update sketch.js

--- demos/js/sketch.js
+++ demos/js/sketch.js
@@ -1,5 +1,5 @@
 let mic, fft;
-let windowResized;
+
 function setup() {
   createCanvas(710, 400);
   fill(23, 166, 199);
@@ -9,15 +9,9 @@ function setup() {
   fft = new p5.FFT();
   fft.setInput(mic);
 }
-function setup() {
-  createCanvas(1280, 250);
-}
-
-
 
 function draw() {
   background(31, 33, 36);
-
 
   let spectrum = fft.analyze();
 
