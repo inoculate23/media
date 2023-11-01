08a599bd847205c49d2d6c255d96a4893dbb9d13
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-11-01 12:03:11 -0700

    Add files via upload

--- demos/js/sketch.js
+++ demos/js/sketch.js
@@ -1,48 +1,23 @@
-let osc, envelope, fft;
-
-let scaleArray = [60, 62, 64, 65, 67, 69, 71, 72];
-let note = 0;
+let mic, fft;
 
 function setup() {
-  createCanvas(800, 159);
-  osc = new p5.SinOsc();
+  createCanvas(710, 400);
+  fill(23, 166, 199);
 
-  // Instantiate the envelope
-  envelope = new p5.Env();
-
-  // set attackTime, decayTime, sustainRatio, releaseTime
-  envelope.setADSR(0.001, 0.5, 0.1, 0.5);
-
-  // set attackLevel, releaseLevel
-  envelope.setRange(1, 0);
-
-  osc.start();
-
+  mic = new p5.AudioIn();
+  mic.start();
   fft = new p5.FFT();
-  noStroke();
+  fft.setInput(mic);
 }
 
 function draw() {
-background(31, 33, 36);
-  
-noStroke();
-  smooth;
+  background(31, 33, 36);
 
-  if (frameCount % 20 === 0 || frameCount === 1) {
-    let midiValue = scaleArray[note];
-    let freqValue = midiToFreq(midiValue);
-    osc.freq(freqValue);
-
-    envelope.play(osc, 0, 0.199);
-    note = (note + 3) % scaleArray.length;
-  }
-
-  // plot FFT.analyze() frequency analysis on the canvas
   let spectrum = fft.analyze();
-  for (let i = 1; i < spectrum.length / 2; i++) {
-    fill(spectrum[i], spectrum[i] /1.5, );
-    let x = map(i, 10, spectrum.length / 55, 150, width);
-    let h = map(spectrum[i], 0, 255, 10, height);
-    rect(x, height, spectrum.length / 40, -h);
+
+  beginShape();
+  for (i = 0; i < spectrum.length; i++) {
+    vertex(i, map(spectrum[i], 20, 205, height, 100));
   }
+  endShape();
 }
